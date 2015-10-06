b73f94c40a686f80c05891695bf90a6722761402
Subv <subv2112@gmail.com>
2015-10-06 00:12:57 -0500

    Añadido modulo de estadisticas. (Work in progress)

---
+++ CineQuilla/Statistics.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Statistics.aspx.cs" Inherits="CineQuilla.Statistics" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <asp:Label Visible="false" ForeColor="Red" runat="server" ID="ErrorLabel" Text="Something went wrong"></asp:Label>
+    <br />
+    Top 10 peliculas mas vistas: <br />
+    <asp:Label Visible="false" ForeColor="Red" runat="server" ID="NoPopularMovies" Text="Nada que mostrar"></asp:Label>
+    <asp:GridView ID="MostViewedMoviesGrid" runat="server"></asp:GridView>
+</asp:Content>
---
+++ CineQuilla/Statistics.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CineQuilla
+{
+    public partial class Statistics : System.Web.UI.Page
+    {
+        public Auth.UserInfo UserInfo = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UserInfo = Session["UserInfo"] as Auth.UserInfo;
+
+            if (UserInfo == null)
+            {
+                Response.Redirect("~/Login.aspx", true);
+                return;
+            }
+
+            if (UserInfo.PermissionLevel <= 1)
+            {
+                Session["ShowPermissionError"] = true;
+                Response.Redirect("~/Default.aspx", true);
+                return;
+            }
+
+            if (!Page.IsPostBack)
+            {
+                try
+                {
+                    var connString = ConfigurationManager.ConnectionStrings["CineQuilla"].ConnectionString;
+                    using (SqlConnection connection = new SqlConnection(connString))
+                    {
+                        connection.Open();
+                        using (SqlCommand command = new SqlCommand(null, connection))
+                        {
+                            command.CommandText = @"SELECT id, name, image, views FROM movies m INNER JOIN (SELECT TOP 5 s.movie_id, COUNT(c.show_id) as views FROM shows s INNER JOIN chairs c ON c.show_id = s.id GROUP BY s.movie_id ORDER BY COUNT(c.show_id)) counts ON counts.movie_id = m.id ORDER BY views DESC";
+
+                            SqlDataAdapter da = new SqlDataAdapter(command);
+                            DataTable dt = new DataTable();
+                            int rows = da.Fill(dt);
+                            if (rows == 0)
+                            {
+                                MostViewedMoviesGrid.DataSource = null;
+                                NoPopularMovies.Visible = true;
+                            }
+                            else
+                            {
+                                MostViewedMoviesGrid.DataSource = dt;
+                                MostViewedMoviesGrid.DataBind();
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    ErrorLabel.Visible = true;
+                }
+            }
+        }
+    }
+}
